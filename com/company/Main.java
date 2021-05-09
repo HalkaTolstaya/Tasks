6ddc7e5494c68f6625886663cac22a1a4c5da04f
HalkaTsta <af@fetter.ru>
2021-05-09 15:28:27 +0300

    f

---
+++ com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import static com.company.Task1.remainder;
+import static com.company.Task1.triArea;
+import static com.company.Task3.checkPerfect;
+import static com.company.Task3.solutions;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(triArea(4,2));
+        System.out.println(checkPerfect(4));
+        // для вызова любого таска просто вставить название таска с параметрами в вывод
+    }
+}
